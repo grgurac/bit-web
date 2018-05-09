77556267c1511dc9c90f1334951cf54016189c51
BIT Student <classroom@bgit.rs>
2018-05-09 12:56:48 +0200

    Bootstrap vezbe

---
+++ bootstrap_vjezba/main.js
@@ -0,0 +1,13 @@
+
+
+var PI = 3.1;
+
+function circleSurface(radius) {
+   var result = radius * radius * PI;
+   var PI = 3.14159;
+  
+   return result;
+}
+
+var output = circleSurface(5);
+console.log(output);
